f8d4a6a59b9615ae13eb2d37cc2ae4503be439c4
Wei Su <willsuwei@hotmail.com>
2019-12-29 13:00:04 -0800

    .

--- Gomoku_MPI/2_train_mpi_hpc.sh
+++ Gomoku_MPI/2_train_mpi_hpc.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+#
+#SBATCH --job-name=pl2ap
+#SBATCH --output=pl2ap-srun.log
+#
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=14
+#SBATCH --time=23:00
+#SBATCH --mem-per-cpu=4000
+#SBATCH --mail-user=username@sjsu.edu
+#SBATCH --mail-type=END
+export OMP_NUM_THREADS=4
+export OMP_PLACES=cores
+export OMP_PROC_BIND=spread
+
 # srun -p gpu --gres=gpu --pty /bin/bash
 
 source ./venv/bin/activate
