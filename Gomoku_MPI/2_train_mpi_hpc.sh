10ea9b19da493b30c17ccb40dbd468bf8d8d1aa1
Wei Su <willsuwei@hotmail.com>
2019-12-29 04:33:35 -0800

    .

--- Gomoku_MPI/2_train_mpi_hpc.sh
+++ Gomoku_MPI/2_train_mpi_hpc.sh
@@ -1,4 +1,4 @@
-srun -p gpu --gres=gpu --pty /bin/bash
+# srun -p gpu --gres=gpu --pty /bin/bash
 
 source ./venv/bin/activate
 mpiexec --oversubscribe -np 3 python -u train_mpi.py
--- Gomoku_MPI/2_train_mpi_hpc_cpu.sh
+++ Gomoku_MPI/2_train_mpi_hpc_cpu.sh
@@ -1,4 +1,4 @@
-srun -n 1 -N 1 -c 14 --pty /bin/bash
+# srun -n 1 -N 1 -c 14 --pty /bin/bash
 
 source ./venv/bin/activate
 mpiexec --oversubscribe -np 10 python -u train_mpi.py
